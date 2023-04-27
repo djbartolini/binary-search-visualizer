489bc8cf0c54d38720c68911874bdd96d47c761d
Daniel Bartolini <djb3149@gmail.com>
2023-04-27 15:50:30 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import Array from './components/Array.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Binary Search Visualizer</h1>
+  </header>
+
+  <main>
+    <h2>Array:</h2>
+    <Array />
+    <div class="start-button-div">
+      <button>Start</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  header {
+    display: flex;
+  }
+  main {
+    min-height: 100vh;
+  }
+  .start-button-div {
+    position: absolute;
+    bottom: 20%;
+  }
+</style>
---
+++ src/components/Array.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  const array = [1, 3, 7, 10, 13, 15, 17, 18, 20, 21, 22, 23, 25, 33, 35, 42, 45, 47, 50, 62]
+</script>
+
+<template>
+  <div class="array-div">
+    <div v-for="(num, i) in array">
+      <div class="box">
+        {{ num }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .array-div {
+    margin-top: 25%;
+    display: flex;
+    flex-direction: row;
+  }
+  .box {
+    border: 1px solid white;
+    padding: 8px;
+    width: 40px;
+    height: 40px;
+  }
+</style>
